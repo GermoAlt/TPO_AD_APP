5d35869909096e5b43a8ed95a9d85ca49611866d
German <germanaltairac@hotmail.com>
2021-09-28 02:49:13 -0300

    setup

---
+++ app/src/main/java/com/ad/grupo10/gestionbarrial/landing/LandingActivity.kt
@@ -0,0 +1,27 @@
+package com.ad.grupo10.gestionbarrial.landing
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.ad.grupo10.gestionbarrial.R
+import com.ad.grupo10.gestionbarrial.landing.onboarding.OnboardingActivity
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_landing)
+
+        val botonComenzar = findViewById<Button>(R.id.button_comenzar)
+        val intentComenzar = Intent(this, OnboardingActivity::class.java)
+        botonComenzar.setOnClickListener{
+            startActivity(intentComenzar)
+        }
+    }
+
+    init {
+
+    }
+}
---
+++ app/src/main/java/com/ad/grupo10/gestionbarrial/landing/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.ad.grupo10.gestionbarrial.landing.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ad.grupo10.gestionbarrial.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
