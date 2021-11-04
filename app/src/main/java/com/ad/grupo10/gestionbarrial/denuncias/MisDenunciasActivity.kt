8cace5e5770d5e8cab703e40da667465022f124e
German Altairac <saltairac@uade.edu.ar>
2021-11-03 21:11:27 -0300

    import

--- app/src/main/java/com/ad/grupo10/gestionbarrial/denuncias/MisDenunciasActivity.kt
+++ app/src/main/java/com/ad/grupo10/gestionbarrial/denuncias/MisDenunciasActivity.kt
@@ -2,6 +2,7 @@ package com.ad.grupo10.gestionbarrial.denuncias
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ad.grupo10.gestionbarrial.R
 
 class MisDenunciasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
